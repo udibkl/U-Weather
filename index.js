d2c40358a42a6b8bc42acb9bdf40d03a64613db9
Udith Bekal <68611381+udibkl@users.noreply.github.com>
2021-08-29 18:13:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ async function getData() {
 
     var inn = inp.value;
    
-    // const url = "http://api.openweathermap.org/data/2.5/weather?q=" + inn + "&units=metric&appid=117a0138213c6e23090a77c42952f2e1";
-    // const url = "http://api.openweathermap.org/data/2.5/weather?q="+inn+"&units=metric&appid=c9dbb6a427mshca586e9cc50c6a7p14563fjsn7c08e5e97552";
+   
     const url="http://api.openweathermap.org/data/2.5/weather?q="+inn+"&units=metric&APPID=084bcacee012feaf3cbf41d20ddc0fbd";
     const result = await fetch(url);
     const data = await result.json();
@@ -20,14 +19,10 @@ async function getData() {
     console.log(data.weather[0].main);
     console.log(data.main.humidity);
     console.log(data.wind.speed);
-    // info.innerHTML = "<span id='p1'>"+ data.name +" </span>"+
-    //     "<br> "+data.weather[0].main+ 
-    //     "<br> humidity: "+data.main.humidity+
-    //     "<br> Wind: "+data.wind.speed+
-    //     "<br><di v id='icon'><img src='http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png'></di>";
+
 
     cel.innerHTML="<span id='cel'>"+data.main.temp+"&deg;C</span>"+" <span><img  id='icon' src='https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png'></span>";
-    // cel.style.
+ 
 humi.innerHTML="<div>Humidity: "+ data.main.humidity +"<br><span>Feels like:" +data.main.feels_like+"&deg;C</span><br><span>Wind speed: "+data.wind.speed+" mph</div>"
 head.innerHTML="<span id='headd'>"+data.name+", "+data.sys.country+"</span><br><span id='desc'>"+data.weather[0].main+" - "+data.weather[0].description+"</span>";
 
